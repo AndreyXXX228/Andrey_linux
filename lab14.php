dd1aeb7273ec5c8be476b7c98e427c2509033c05
AndreyXXX228 <secondakk002.com>
2026-04-08 15:51:57 +0300

    добавлена blog page

--- lab14.php
+++ lab14.php
@@ -16,5 +16,29 @@ class Page
     }
 }
 
-$page = new Page('page', '<div><p>It is a default page</p></div>');
-$page->render();
+class BlogPage extends Page
+{
+    public function __construct()
+    {
+        $template = <<<HTML
+        <div style="display: flex; gap: 20px; flex-wrap: wrap;">
+            <div style="border:1px solid #ccc; padding:10px; width:200px;">
+                <h3>Новость 1</h3>
+                <p>Краткое описание первой новости.</p>
+            </div>
+            <div style="border:1px solid #ccc; padding:10px; width:200px;">
+                <h3>Новость 2</h3>
+                <p>Краткое описание второй новости.</p>
+            </div>
+            <div style="border:1px solid #ccc; padding:10px; width:200px;">
+                <h3>Новость 3</h3>
+                <p>Краткое описание третьей новости.</p>
+            </div>
+        </div>
+        HTML;
+        parent::__construct('blog', $template);
+    }
+}
+
+$blog = new BlogPage();
+$blog->render();
